76b9b4d6e424206d63191b5814a8e9c65da0f447
SeptEarlyMorning <1790732099@qq.com>
2022-05-31 11:43:52 +0800

    feat: 00189-easy-awaited

---
+++ questions/00189-easy-awaited.ts
@@ -0,0 +1,26 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+type Z = Promise<Promise<string | number>>;
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>
+];
+
+// @ts-expect-error
+type error = MyAwaited<number>;
+
+// ============= Your Code Here =============
+/**
+ * 1. 使用递归。
+ * 2. infer 取出 Promise 里的类型。
+ */
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer U>
+  ? U extends Promise<unknown>
+    ? MyAwaited<U>
+    : U
+  : never;
