626bbdfa30f14592798c6f4da5c284970db23218
SeptEarlyMorning <1790732099@qq.com>
2022-06-05 18:43:24 +0800

    feat: 00612-medium-kebabcase

---
+++ questions/00612-medium-kebabcase.ts
@@ -0,0 +1,49 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<KebabCase<"FooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"fooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"foo-bar">, "foo-bar">>,
+  Expect<Equal<KebabCase<"foo_bar">, "foo_bar">>,
+  Expect<Equal<KebabCase<"Foo-Bar">, "foo--bar">>,
+  Expect<Equal<KebabCase<"ABC">, "a-b-c">>,
+  Expect<Equal<KebabCase<"-">, "-">>,
+  Expect<Equal<KebabCase<"">, "">>,
+  Expect<Equal<KebabCase<"😎">, "😎">>
+];
+
+// ============= Your Code Here =============
+type Dictionary = {
+  A: "a";
+  B: "b";
+  C: "c";
+  D: "d";
+  E: "e";
+  F: "f";
+  G: "g";
+  H: "h";
+  I: "i";
+  J: "j";
+  K: "k";
+  L: "l";
+  M: "m";
+  N: "n";
+  O: "o";
+  P: "p";
+  Q: "q";
+  R: "r";
+  S: "s";
+  T: "t";
+  U: "u";
+  V: "v";
+  W: "w";
+  X: "x";
+  Y: "y";
+  Z: "z";
+};
+type KebabCase<S, P extends 0 | 1 = 0> = S extends `${infer First}${infer Rest}`
+  ? `${First extends keyof Dictionary
+      ? `${P extends 1 ? "-" : ""}${Dictionary[First]}`
+      : First}${KebabCase<Rest, 1>}`
+  : S;
