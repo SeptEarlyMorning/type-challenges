03745fc53ec775c5940a9682c55626792b55addc
SeptEarlyMorning <1790732099@qq.com>
2022-06-06 17:22:50 +0800

    feat: 00949-medium-anyof

---
+++ questions/00949-medium-anyof.ts
@@ -0,0 +1,27 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<
+    Equal<AnyOf<[1, "test", true, [1], { name: "test" }, { 1: "test" }]>, true>
+  >,
+  Expect<Equal<AnyOf<[1, "", false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "test", false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", true, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [1], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [], { name: "test" }]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [], { 1: "test" }]>, true>>,
+  Expect<
+    Equal<AnyOf<[0, "", false, [], { name: "test" }, { 1: "test" }]>, true>
+  >,
+  Expect<Equal<AnyOf<[0, "", false, [], {}]>, false>>,
+  Expect<Equal<AnyOf<[]>, false>>
+];
+
+// ============= Your Code Here =============
+/**
+ * 注意：判断是否为 {} 类型应使用 { [P in any]: never }，不能使用 {}。
+ */
+type Falsy = 0 | "" | false | [] | { [P in any]: never };
+
+type AnyOf<T extends readonly any[]> = T[number] extends Falsy ? false : true;
