9051e4deb4e65cbb796961a3382e3d1a91f45f9d
SeptEarlyMorning <1790732099@qq.com>
2022-06-14 15:28:53 +0800

    feat: 04499-medium-chunk

---
+++ questions/04499-medium-chunk.ts
@@ -0,0 +1,20 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Chunk<[], 1>, []>>,
+  Expect<Equal<Chunk<[1, 2, 3], 1>, [[1], [2], [3]]>>,
+  Expect<Equal<Chunk<[1, 2, 3], 2>, [[1, 2], [3]]>>,
+  Expect<Equal<Chunk<[1, 2, 3, 4], 2>, [[1, 2], [3, 4]]>>,
+  Expect<Equal<Chunk<[1, 2, 3, 4], 5>, [[1, 2, 3, 4]]>>,
+  Expect<Equal<Chunk<[1, true, 2, false], 2>, [[1, true], [2, false]]>>
+];
+
+// ============= Your Code Here =============
+type Chunk<T, U, P extends unknown[] = []> = P["length"] extends U
+  ? [P, ...Chunk<T, U, []>]
+  : T extends [infer First, ...infer Rest]
+  ? Chunk<Rest, U, [...P, First]>
+  : P extends []
+  ? []
+  : [P];
