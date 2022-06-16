2d600d6a97e03ba2195c2bf9243cd10a74db5c51
SeptEarlyMorning <1790732099@qq.com>
2022-06-17 00:00:02 +0800

    feat: 00223-hard-isany

---
+++ questions/00223-hard-isany.ts
@@ -0,0 +1,14 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<IsAny<any>, true>>,
+
+  Expect<Equal<IsAny<undefined>, false>>,
+  Expect<Equal<IsAny<unknown>, false>>,
+  Expect<Equal<IsAny<never>, false>>,
+  Expect<Equal<IsAny<string>, false>>
+];
+
+// ============= Your Code Here =============
+type IsAny<T> = boolean extends (T extends 1 ? true : false) ? true : false;
