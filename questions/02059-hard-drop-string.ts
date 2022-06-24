b7309c68b543c437dbd8c77177aafef8e306171e
SeptEarlyMorning <1790732099@qq.com>
2022-06-24 15:41:53 +0800

    feat: 02059-hard-drop-string

---
+++ questions/02059-hard-drop-string.ts
@@ -0,0 +1,30 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<DropString<"butter fly!", "">, "butter fly!">>,
+  Expect<Equal<DropString<"butter fly!", " ">, "butterfly!">>,
+  Expect<Equal<DropString<"butter fly!", "but">, "er fly!">>,
+  Expect<
+    Equal<DropString<" b u t t e r f l y ! ", "but">, "     e r f l y ! ">
+  >,
+  Expect<Equal<DropString<"    butter fly!        ", " ">, "butterfly!">>,
+  Expect<Equal<DropString<" b u t t e r f l y ! ", " ">, "butterfly!">>,
+  Expect<
+    Equal<DropString<" b u t t e r f l y ! ", "but">, "     e r f l y ! ">
+  >,
+  Expect<
+    Equal<DropString<" b u t t e r f l y ! ", "tub">, "     e r f l y ! ">
+  >,
+  Expect<
+    Equal<DropString<" b u t t e r f l y ! ", "b">, "  u t t e r f l y ! ">
+  >,
+  Expect<Equal<DropString<" b u t t e r f l y ! ", "t">, " b u   e r f l y ! ">>
+];
+
+// ============= Your Code Here =============
+type DropString<S, R extends string> = R extends `${infer A}${infer B}`
+  ? S extends `${infer C}${A}${infer Rest}`
+    ? DropString<`${C}${DropString<Rest, A>}`, B>
+    : S
+  : S;
